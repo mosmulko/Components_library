fed952338677101f0c26fe49e59f70313ce686a5
Monika Smulko <monika.smulko@gmail.com>
2021-07-08 15:21:47 +0200

    Add packages and config files

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+        resolve: {
+          extensions: [".js", ".jsx"],
+        },
+      },
+    ],
+  },
+};
